f231566a6ceadf34ccaa88af4975ef9d9912db29
Razvan <razvan99h@yahoo.com>
2018-11-24 19:05:55 +0200

    scos pagini goale

--- BWay/src/app/app.component.ts
+++ BWay/src/app/app.component.ts
@@ -18,6 +18,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      console.log("ba pula");
       statusBar.styleDefault();
       splashScreen.hide();
     });
