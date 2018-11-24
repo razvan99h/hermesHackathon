e574792e648af58241abb29e925661dc91aac9c2
RusuRaul <raulrusu2014@gmail.com>
2018-11-24 16:27:29 +0200

    Ionic- FireBase integration

--- BWay/src/app/app.module.ts
+++ BWay/src/app/app.module.ts
@@ -3,9 +3,20 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAEm_npBxolAXgfj4Muwae7AwEuZISsPzQ",
+  authDomain: "bway-66566.firebaseapp.com",
+  databaseURL: "https://bway-66566.firebaseio.com",
+  storageBucket: "bway-66566.appspot.com",
+  messagingSenderId: "919488996075"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- BWay/src/pages/home/home.ts
+++ BWay/src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  items
+  constructor(
+    public navCtrl: NavController,
+    public db: AngularFireDatabase
+  )
+  {
+      this.retriveAll()
   }
 
+
+  retriveAll()
+  {
+    this.db.object('/item').valueChanges().subscribe(data =>
+    {
+      console.log(data)
+    })
+
+  }
 }
